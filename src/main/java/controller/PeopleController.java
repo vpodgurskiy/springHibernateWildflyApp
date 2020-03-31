933aaa8c36f8c6347178485ff4a0f713cdfd695e
vpodgurskiy <vpodgurskiy@gmail.com>
2020-03-31 16:56:03 +0300

    add new web page for creating people.

--- src/main/java/controller/PeopleController.java
+++ src/main/java/controller/PeopleController.java
@@ -3,10 +3,13 @@ package controller;
 import model.People;
 import model.PeopleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.Map;
 
 public class PeopleController {
     @Autowired
@@ -18,5 +21,18 @@ public class PeopleController {
         ModelAndView mav = new ModelAndView("index");
         mav.addObject("listPeople", listPeople);
         return mav;
+    }
+
+    @RequestMapping("/new")
+    public String newCustomerForm(Map<String, Object> model) {
+        People people = new People();
+        model.put("people", people);
+        return "new_people";
+    }
+
+    @RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String saveCustomer(@ModelAttribute("people") People people) {
+        peopleService.save(people);
+        return "redirect:/";
     }
 }
